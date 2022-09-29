1a9ed57e8c21f3d31c8b18657446f1c78ae43015
Your Name <you@example.com>
2022-09-29 01:47:43 -0700

    My first commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include main.h
 /**
  * _puts_recursion - Prints a string followed by a new line
  * @s: string
@@ -8,11 +8,11 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == 0)
-	{
-		_putchar('\n');
-		return;
-	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+        if (*s == 0)
+        {
+                _putchar('\n');
+                return;
+        }
+        _putchar(*s);
+        _puts_recursion(s + 1);
 }
