f6d4622a888e3846f221337ad87858ca72787f5e
Your Name <you@example.com>
2022-10-25 03:11:55 -0700

    My first commit

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,40 +1,42 @@
 #include "lists.h"
+
 /**
- * add_nodeint_end - this function adds a new node at the end of a list
- * @head: the pointer that points to the struct
- * @n: integer in the struct
- *
- * Return: the adress of the new element
+ * add_nodeint_end - function that adds a new node at the end of a
+ * listint_t list
+ * @head: Const double pointer of structure list_t for beginning
+ * @n: Const int to be added
+ * Return: List with new node for listint_t list, NULL if failed
  */
+
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *newnode;
-	listint_t *point;
 
-	if (&*head == NULL)
-	{
+	listint_t *new;
+	listint_t *temp;
+
+	new  = malloc(sizeof(listint_t));
+
+	if (new == NULL)
 		return (NULL);
-	}
-	newnode = malloc(sizeof(listint_t));
-	if (newnode == NULL)
-	{
-		return (NULL);
-	}
-	newnode->n = n;
-	newnode->next = NULL;
+
 	if (*head == NULL)
 	{
-		*head = newnode;
+		*head = new;
+		new->n = n;
+		new->next = NULL;
+		return (new);
 	}
-	else
-	{
-		point = *head;
 
-		while (point->next != NULL)
-		{
-			point = point->next;
-		}
-		point->next = newnode;
-	}
-	return (*head);
+	temp = *head;
+
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	temp->next = new;
+
+	new->n = n;
+	new->next = NULL;
+
+	return (new);
+
 }
