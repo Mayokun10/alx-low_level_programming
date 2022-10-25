0095aa7dcc829ebeecc7df81088e72ba91ecdcad
Your Name <you@example.com>
2022-10-25 03:16:51 -0700

    My first commit

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,23 +1,28 @@
 #include "lists.h"
+
 /**
- * pop_listint - this function deletes the head node of a list
- * @head: the pointer to the struct
- *
- * Return: head node's data
+ * pop_listint - function that deletes the head node of a listint_t linked
+ * list, and returns the head node’s data (n)
+ * @head: Const double pointer of structure listint_t for beginning
+ * Return: Int for data from head node to be deleted
  */
+
 int pop_listint(listint_t **head)
 {
-	listint_t *fr;
-	int nodedata;
+	int n;
 
-	if (*head == NULL)
-	{
+	listint_t *temp;
+
+	if (head == NULL || *head == NULL)
 		return (0);
-	}
 
-	nodedata = (*head)->n;
-	fr = (*head)->next;
-	free(*head);
-	*head = fr;
-	return (nodedata);
+	n = (*head)->n;
+
+	temp = *head;
+
+	*head = (*head)->next;
+
+	free(temp);
+
+	return (n);
 }
