c3b7992a6ac2e7c4db48c105c91722bd9da30ecf
Your Name <you@example.com>
2022-10-25 03:23:43 -0700

    My first commit

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,38 +1,53 @@
 #include "lists.h"
+
 /**
- * delete_nodeint_at_index - deletes the node at index of a linked list
- * @head: the pointer to the struct
- * @index: index of the node that should be deleted
- *
- * Return: 1 if it succeeded, -1 if it failed
+ * delete_nodeint_at_index - function that deletes the node at index index of a
+ * listint_t linked list
+ * @head: Double pointer of linked list listint_t
+ * @index: Unsigned int index of the list where the new node should be deleted.
+ * Index starts at 0
+ * Return: 1 if success, -1 if failed
  */
+
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *tmp, *pretmp = *head;
-	unsigned int i;
+	listint_t *temp1;
+	listint_t *temp2;
+	listint_t *prev;
 
-	if (pretmp == NULL)
-	{
+	unsigned int i = 0;
+
+	prev = *head;
+
+	if (head == NULL || *head == NULL)
 		return (-1);
-	}
 
-	if (index == 0)
+	if (index == 0 && (*head)->next != NULL)
 	{
-		*head = (*head)->next;
-		free(pretmp);
+		prev = *head;
+		*head = prev->next;
+		free(prev);
 		return (1);
 	}
 
-	for (i = 0 ; i < (index - 1) ; i++)
+	if (index == 0 && (*head)->next == NULL)
 	{
-		if (pretmp->next == NULL)
-		{
-			return (-1);
-		}
-		pretmp = pretmp->next;
+		temp1 = *head;
+		*head = (*head)->next;
+		free(temp1);
+		return (1);
 	}
-	tmp = pretmp->next;
-	pretmp->next = tmp->next;
-	free(tmp);
-	return (1);
+
+	for (i = 0, prev = *head; prev != NULL; i++, prev = prev->next)
+	{
+		if ((index - 1) == i)
+		{
+			temp1 = prev->next;
+			temp2 = temp1->next;
+			prev->next = temp2;
+			free(temp1);
+			return (1);
+		}
+	}
+	return (-1);
 }
