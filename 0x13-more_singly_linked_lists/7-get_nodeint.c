44e4d59cc747ddcd46363b7fefccb91ffeccf336
Your Name <you@example.com>
2022-10-25 03:18:15 -0700

    My first commit

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,23 +1,44 @@
 #include "lists.h"
+
 /**
- * get_nodeint_at_index - return the nth node of a linked list
- * @head: the pointer to the struct
- * @index: index of the node
- *
- * Return: the nth node of a linked list
+ * get_nodeint_at_index - function that returns the nth node of a listint_t
+ * linked list
+ * @head: Pointer of structure listint_t for beginning
+ * @index: Int of index of the node, starting at 0
+ * Return: Nth node of a listint_t linked list, NULL if node does not exist
  */
+
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int check = 0;
 
-	while (head != NULL)
+	listint_t *temp;
+	listint_t *temp2;
+	unsigned int i = 0, j = 0;
+
+	if (head == NULL)
+		return (NULL);
+
+	temp2 = head;
+
+	while (temp2 != NULL)
 	{
-		if (check == index)
-		{
-			return (head);
-		}
-		check++;
-		head = head->next;
+		temp2 = temp2->next;
+		j++;
+
 	}
-	return (NULL);
+
+	if (index > j)
+		return (NULL);
+
+	while (i < index)
+	{
+		temp = head->next;
+
+		if (temp == NULL)
+			return (NULL);
+
+		head = temp;
+		i++;
+	}
+	return (head);
 }
