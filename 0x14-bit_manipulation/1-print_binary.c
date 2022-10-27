e43b287078293869508b21faff49bd93aa468205
Your Name <you@example.com>
2022-10-27 11:17:53 -0700

    My first commit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,14 +1,16 @@
 #include "main.h"
 /**
- * print_binary - function that prints the binary representation of a number
- * @n: Unsigned int n to be converted
- * Return: void
+ * print_binary - prints the binary representation of a number
+ * @n: a string of 0 and 1 chars
+ *
  */
-
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
-		print_binary(n >> 1);
+	int printbin = ((n & 1) + '0');
 
-		_putchar((n & 1) + '0');
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	_putchar(printbin);
 }
