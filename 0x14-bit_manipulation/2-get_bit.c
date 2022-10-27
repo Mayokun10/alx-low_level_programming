2d8c184e4a57bfb00938107609f1849aebb344bc
Your Name <you@example.com>
2022-10-27 11:20:02 -0700

    My first commit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * get_bit - function that returns the value of a bit at a given index
+ * @n: Unsigned long int n for binary passed
+ * @index: Unsigned int for index to be retreived
+ * Return: The value of the bit at index index or -1 if an error occured
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask = 1;
+
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	mask = (mask << index);
+
+	if ((mask & n) == 0)
+		return (0);
+
+	else
+		return (1);
+
+}
